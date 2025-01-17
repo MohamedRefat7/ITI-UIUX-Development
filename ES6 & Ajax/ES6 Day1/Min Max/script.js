26cf9a09ec2fe71b84a402cd64f579fc1888ef29
Mohamed Refat <141337065+MohamedRefat7@users.noreply.github.com>
2025-01-17 20:50:10 +0200

    Delete ES6 & Ajax directory

--- ES6 & Ajax/ES6 Day1/Min Max/script.js
+++
@@ -1,5 +0,0 @@
-function minMax(arr) {
-  return [Math.min(...arr), Math.max(...arr)];
-}
-
-console.log(minMax([1, 2, 3, 4, 20, 5, 6, 7, 8, 9]));
--- ES6 & Ajax/ES6 Day1/Swap Values/script.js
+++
@@ -1,9 +0,0 @@
-let a = 10;
-let b = 20;
-
-console.log(a);
-console.log(b);
-[a, b] = [b, a];
-
-console.log(a);
-console.log(b);
